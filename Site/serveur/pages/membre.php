9881656b0ff4a98621b073f014a5eba3d3ba58c6
Imoen148 <100290514+Imoen148@users.noreply.github.com>
2023-01-27 10:38:39 -0500

    cmd php

--- Site/serveur/pages/membre.php
+++ Site/serveur/pages/membre.php
@@ -27,7 +27,6 @@
 
 	<!-- Custom stlylesheet -->
 	<link type="text/css" rel="stylesheet" href="../../client/css/style.css" />
-	<link rel="stylesheet" href="../../client/js/test.js">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
