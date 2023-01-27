322fa1546686a8b26c1d69e2446b7951196c4615
David Normandin <dvdnormans@hotmail.com>
2023-01-26 20:42:06 -0500

    Revert "Update enregMembre.php"
    This reverts commit 9e934d7ed8e155f6c4775c4aa2c707a362097fda.

--- Site/serveur/enregMembre.php
+++ Site/serveur/enregMembre.php
@@ -1,6 +1,19 @@
 <?php
 
-require_once("includes/configdb.inc.php");
+$servername = "sql9.freesqldatabase.com";
+$username = "sql9558434";
+$password = "bQV64kWUMF";
+$dbname = "sql9558434";
+ 
+// Create connection
+$conn = mysqli_connect($servername,
+    $username, $password, $dbname);
+ 
+// Check connection
+if ($conn === false) {
+    die("Connection failed: "
+        . mysqli_connect_error());
+}
 
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
@@ -11,40 +24,54 @@ $mdp = $_POST['mdp'];
 $cmdp = $_POST['cmdp'];
 
 
-// SQL INSERT pour la table membres
+echo $daten;
 
-$sql = "INSERT INTO membres VALUES
-    ('$nom', '$prenom', '$email', '$sexe', '$daten')";
+if($mdp == $cmdp){
+    $s = "";
+    if($sexe == "Homme"){
+        $s = "M";
+    }else if($sexe == "Femme"){
+        $s = "F";
+    }else if($sexe == "Autre"){
+        $s = "A";
+    }
 
-if(mysqli_query($conn, $sql)){
-    echo "<h3>data stored in a database successfully."
-        . " Please browse your localhost php my admin"
-        . " to view the updated data</h3>";
-
-    echo nl2br("Vous pouvez maintenant vous connectez en utilisant ".$email." comme identifiant.");
-} else{
-    echo "ERROR: Hush! Sorry $sql. "
-        . mysqli_error($conn);
-}
-
-// SQL INSERT pour la table connexion
-$sql = "INSERT INTO connexion VALUES
-    ('$email', '$mdp', 'M', 'A')";
-
-if(mysqli_query($conn, $sql)){
-    echo "<h3>data stored in a database successfully."
-        . " Please browse your localhost php my admin"
-        . " to view the updated data</h3>";
+    // SQL INSERT pour la table membres
+    
+    $sql = "INSERT INTO membres VALUES
+        ('$nom', '$prenom', '$email', '$s', '$daten')";
+    
+    if(mysqli_query($conn, $sql)){
+       echo "<h3>data stored in a database successfully."
+           . " Please browse your localhost php my admin"
+           . " to view the updated data</h3>";
+    
+       echo nl2br("Vous pouvez maintenant vous connectez en utilisant ".$email." comme identifiant.");
     } else{
-    echo "ERROR: Hush! Sorry $sql. "
-        . mysqli_error($conn);
+       echo "ERROR: Hush! Sorry $sql. "
+           . mysqli_error($conn);
     }
     
-// Close connection
-mysqli_close($conn);
+    // SQL INSERT pour la table connexion
+    $sql = "INSERT INTO connexion VALUES
+        ('$email', '$mdp', 'M', 'I')";
+    
+    if(mysqli_query($conn, $sql)){
+        echo "<h3>data stored in a database successfully."
+            . " Please browse your localhost php my admin"
+            . " to view the updated data</h3>";
+     } else{
+        echo "ERROR: Hush! Sorry $sql. "
+            . mysqli_error($conn);
+     }
+     
+    // Close connection
+    mysqli_close($conn);
 
 
-
+}else{
+    echo "Les mots de passe diffère.";
+}
 
 
 ?>
