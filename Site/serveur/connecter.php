ad4b2e672862110b819bd8840e03a4410394b04a
Imoen148 <100290514+Imoen148@users.noreply.github.com>
2023-01-29 10:46:03 -0500

    dernier bug

--- Site/serveur/connecter.php
+++ Site/serveur/connecter.php
@@ -1,59 +1,45 @@
 <?php
-function write_to_console($data)
-{
-    $console = $data;
-    if (is_array($console))
-        $console = implode(',', $console);
-
-    echo "<script>console.log('Console: " . $console . "' );</script>";
-}
-
-// echo '<script>alert("OUAIP!!!")</script>';
-// die();
 
 session_start();
+
 require_once("includes/configdb.inc.php");
 
+$email = $_POST['email'];
+$mdp = $_POST['mdp'];
 
-$email = trim($_POST['email']);
-$mdp = trim($_POST['mdp']);
-write_to_console($email);
-write_to_console($mdp);
 $requete = "SELECT * FROM connexion WHERE email = ? AND mdp = ?";
 $stmt = $conn->prepare($requete);
 $stmt->bind_param("ss", $email, $mdp);
 $stmt->execute();
 $result = $stmt->get_result();
-
 if (!$ligne = $result->fetch_object()) {
-    mysqli_close($conn);
-    // echo '<script type="text/JavaScript">document.getElementById("msgErrConn").innerText = "Membre inexistant...";</script>';
-    write_to_console("Pas trouvé");
-    exit;
-}
-write_to_console("trouvé");
-if ($ligne->statut_m == "A") {
-    $_SESSION['email'] = $email;
-    $_SESSION['role_m'] = $ligne->role_m;
-    $a = $_SESSION['role_m'];
-    write_to_console($a);
-
-    if ($ligne->role_m == "M") {
-        $_SESSION['statut_m'] = 'M';
-        header('Location: pages/membre.php');
-        exit;
-    } elseif ($ligne->role_m == "A") {
-        $_SESSION['statut_m'] = 'A';
-        header('Location: pages/admin.php');
-        exit;
-    }
+    echo "E";
 } else {
-    header('Location: ../../index.php?msg=Problème+avec+votre+compte,+contactez+l\'administrateur');
-    exit;
+    if ($ligne->statut_m == "A") {
+        if ($ligne->role_m == "M") {
+            $_SESSION['statut_m'] = 'M';
+
+            // Aller chercher son prénom :
+            $requete = "SELECT * FROM membres WHERE email = ?";
+            $stmt = $conn->prepare($requete);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if (!$ligne = $result->fetch_object()) {
+                $_SESSION['prenom'] = 'Erreur!';
+            } else {
+                $_SESSION['prenom'] = trim($ligne->prenom);
+            }
+            echo "M";
+        } elseif ($ligne->role_m == "A") {
+            $_SESSION['statut_m'] = 'A';
+            echo "A";
+        }
+    } else {
+        echo "I";
+    }
 }
-
 mysqli_close($conn);
-
 
 
 ?>
