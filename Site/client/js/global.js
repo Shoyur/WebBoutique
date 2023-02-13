f8bb33b0349c897cecebef64116c57ddd59c9f7f
Imoen148 <100290514+Imoen148@users.noreply.github.com>
2023-02-12 22:20:41 -0500

    Update global.js

--- Site/client/js/global.js
+++ Site/client/js/global.js
@@ -1,45 +1,45 @@
-// window.addEventListener("load", function () {
-//     showProduct();
-// });
+window.addEventListener("load", function () {
+    showProduct();
+});
 
-// let showProduct = async () => {
-//     let reponse = await fetch('serveur/getProduits.php');
-//     let responseText = await reponse.text();
-//     // console.log(responseText);
-//     let data = await JSON.parse(responseText);
-//     for (let i = 0; i < data.length; i++) {
-//         let produit = `
-//             <div class="product-img">
-//                 <img src="${data[i].chemin_img.substring(5)}" alt="">
-//                 <div class="product-label">
-//                     <span class="sale">-30%</span>
-//                     <span class="new">NEW</span>
-//                 </div>
-//             </div>
-//             <div class="product-body">
-//                 <p class="product-category">${data[i].categorie}</p>
-//                 <h3 class="product-name"><a href="#">${data[i].nom_prod.substring(0, 15)}</a></h3>
-//                 <h4 class="product-price">$${data[i].prix} <del class="product-old-price">$990.00</del></h4>
-//                 <div class="product-rating">
-//                     <i class="fa fa-star"></i>
-//                     <i class="fa fa-star"></i>
-//                     <i class="fa fa-star"></i>
-//                     <i class="fa fa-star"></i>
-//                     <i class="fa fa-star"></i>
-//                 </div>
-//                 <div class="product-btns">
-//                     <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-//                     <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-//                     <button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-//                 </div>
-//             </div>
-//             <div class="add-to-cart">
-//                 <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-//             </div>`;
-//         let idIndexHTML = "produit" + (i + 1);
-//         document.getElementById(idIndexHTML).innerHTML = produit;
-//     }
-// };
+let showProduct = async () => {
+    let reponse = await fetch('serveur/getProduits.php');
+    let responseText = await reponse.text();
+    // console.log(responseText);
+    let data = await JSON.parse(responseText);
+    for (let i = 0; i < data.length; i++) {
+        let produit = `
+            <div class="product-img">
+                <img src="${data[i].chemin_img}" alt="">
+                <div class="product-label">
+                    <span class="sale">-30%</span>
+                    <span class="new">NEW</span>
+                </div>
+            </div>
+            <div class="product-body">
+                <p class="product-category">${data[i].categorie}</p>
+                <h3 class="product-name"><a href="#">${data[i].nom_prod.substring(0, 15)}</a></h3>
+                <h4 class="product-price">$${data[i].prix} <del class="product-old-price">$990.00</del></h4>
+                <div class="product-rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <div class="product-btns">
+                    <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+                    <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+                    <button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+                </div>
+            </div>
+            <div class="add-to-cart">
+                <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+            </div>`;
+        let idIndexHTML = "produit" + (i + 1);
+        document.getElementById(idIndexHTML).innerHTML = produit;
+    }
+};
 
 let validerFormEnregPartOne = () => {
     const pass = document.getElementById('mdp').value;
@@ -98,8 +98,8 @@ let lister = (listeProduits) => {
                                     <th scope="col">Prix</th>
                                     <th scope="col">Qté total</th>
                                     <th scope="col">Qté vendue</th>
-                                    <th scope="col">Modifier</th>
-                                    <th scope="col">Supprimer</th>
+                                    <th scope="col"></th>
+                                    <th scope="col"></th>
                                 </tr>
                             </thead>
 
@@ -135,15 +135,15 @@ let creerRangee = (unProduit) => {
     `;
 }
 
-// $('.dropdown-toggle').click(function(e) {
-//     if ($(document).width() > 768) {
-//       e.preventDefault(); 
-//       var url = $(this).attr('href');    
-//       if (url !== '#') {
-//         window.location.href = url;
-//       }
-//     }
-// });
+$('.dropdown-toggle').click(function(e) {
+    if ($(document).width() > 768) {
+      e.preventDefault(); 
+      var url = $(this).attr('href');    
+      if (url !== '#') {
+        window.location.href = url;
+      }
+    }
+});
 
 
 
